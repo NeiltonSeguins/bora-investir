f1c881dfd21e4eafa28b637b7ae27c0b5aa1edce
NeiltonSeguins <seguins.neilton@gmail.com>
2022-01-03 17:07:39 -0300

    Cria a funcionalidade de exibir e ocultar a aba de gráficos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-document.addEventListener('DOMContentLoaded', nav)
-function nav(){
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.main-nav');
-    burger.addEventListener('click', ()=>{
-        nav.classList.toggle('show')
-    })
+const btnMostrarMais = document.querySelector(".container-mais");
+const btnMostrarMenos = document.querySelector(".graficos__button--menos");
+const div = document.querySelector("#show-graphs");
+
+document.addEventListener("DOMContentLoaded", nav);
+
+function nav() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".main-nav");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
 }
+
+btnMostrarMais.addEventListener("click", () => {
+  btnMostrarMais.style.display = "none";
+  div.style.display = "flex";
+  btnMostrarMenos.style.display = "block";
+});
+
+btnMostrarMenos.addEventListener("click", () => {
+  btnMostrarMenos.style.display = "none";
+  div.style.display = "none";
+  btnMostrarMais.style.display = "block";
+});
